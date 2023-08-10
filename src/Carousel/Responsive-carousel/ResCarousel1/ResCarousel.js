d81cd647537f4731817336c1238e0fbef72d5bc4
duran550 <duran.mengot@kaeyros-analytics.com>
2023-08-10 20:27:14 +0100

    Layers 4C

--- src/Carousel/Responsive-carousel/ResCarousel1/ResCarousel.js
+++ src/Carousel/Responsive-carousel/ResCarousel1/ResCarousel.js
@@ -1,35 +1,39 @@
-import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
+import items from "./ResCarousel1.json";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "./ResCarousel1.scss";
+
+// const breakPoints = [
+//   { width: 1, itemsToShow: 1 },
+//   { width: 550, itemsToShow: 1, itemsToScroll: 2 },
+//   { width: 768, itemsToShow: 2 },
+//   { width: 1200, itemsToShow: 3 },
+// ];
 
 export default function ResponsiveCarousel1() {
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-          slidesToSlide: 3 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 2 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
-
+  const elastic = items;
   return (
-    <div>
-      <h1>React multi carousel</h1>
-      <Carousel responsive={responsive}>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-      </Carousel> 
-      ;
+    <div className="container">
+      <Carousel 
+        showArrows={true}
+        autoPlay={true}
+        showIndicators={true}
+        infiniteLoop={true}
+        dynamicHeight={false}
+        className="mySwiper"
+      >
+        {elastic.map((item) => (
+          <div key={item.id} className="swipItem">
+            <div className="imgBox">
+              <img src={item.imageUrl} alt="slides" />
+            </div>
+            {/* <div className="detail">
+              <h2>{item.title}</h2>
+              <p>{item.text}</p>
+            </div> */}
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
--- src/components/ProgramItem/ProgramItem.js
+++ src/components/ProgramItem/ProgramItem.js
@@ -1,5 +1,5 @@
 // import { Link } from "react-router-dom";
-import classes from "./ProgramItem.module.scss";
+// import classes from "./ProgramItem.module.scss";
 
 function ProgramItem() {
     return (
